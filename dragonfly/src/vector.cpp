91659d380507439248be4a43e4c50abb5ba2052d
kghite <kghite@gmail.com>
2026-01-06 20:30:51 -0500

    Move to standard google .clang-format

--- dragonfly/src/vector.cpp
+++ dragonfly/src/vector.cpp
@@ -1,52 +1,33 @@
 #include <df/vector.h>
 
-namespace df
-{
+namespace df {
 
-Vector::Vector(float x, float y) : m_x{x}, m_y{y}
-{
+Vector::Vector(float x, float y) : m_x{x}, m_y{y} {}
+
+float Vector::getX() const noexcept { return m_x; }
+
+float Vector::getY() const noexcept { return m_y; }
+
+void Vector::setX(float x) noexcept { m_x = x; }
+
+void Vector::setY(float y) noexcept { m_y = y; }
+
+Vector Vector::operator+(const Vector &other) const noexcept {
+  return Vector{m_x + other.m_x, m_y + other.m_y};
 }
 
-float Vector::getX() const noexcept
-{
-    return m_x;
+Vector &Vector::operator+=(const Vector &other) noexcept {
+  m_x += other.m_x;
+  m_y += other.m_y;
+  return *this;
 }
 
-float Vector::getY() const noexcept
-{
-    return m_y;
+bool Vector::operator==(const Vector &other) const noexcept {
+  return m_x == other.m_x && m_y == other.m_y;
 }
 
-void Vector::setX(float x) noexcept
-{
-    m_x = x;
-}
-
-void Vector::setY(float y) noexcept
-{
-    m_y = y;
-}
-
-Vector Vector::operator+(const Vector& other) const noexcept
-{
-    return Vector{m_x + other.m_x, m_y + other.m_y};
-}
-
-Vector& Vector::operator+=(const Vector& other) noexcept
-{
-    m_x += other.m_x;
-    m_y += other.m_y;
-    return *this;
-}
-
-bool Vector::operator==(const Vector& other) const noexcept
-{
-    return m_x == other.m_x && m_y == other.m_y;
-}
-
-bool Vector::operator!=(const Vector& other) const noexcept
-{
-    return !(*this == other);
+bool Vector::operator!=(const Vector &other) const noexcept {
+  return !(*this == other);
 }
 
 }  // namespace df
