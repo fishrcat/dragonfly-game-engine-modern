244095cb6dd0a65573f46373e5606a4e19613220
kghite <kghite@gmail.com>
2026-01-06 21:18:10 -0500

    Init manager and add build+test target

---
+++ dragonfly/include/manager.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+
+namespace df {
+class Manager {
+    private:
+    std::string m_type;
+    bool m_is_started;
+
+    protected:
+    void setType(std::string type);
+
+    public:
+    Manager();
+    virtual ~Manager();
+
+    std::string getType() const;
+
+    virtual int startUp();
+
+    virtual void shutDown();
+
+    bool isStarted() const;
+};
+
+}  // namespace df
---
+++ dragonfly/tests/manager_test.cpp
@@ -0,0 +1,9 @@
+#include "manager.h"
+
+#include <gtest/gtest.h>
+
+namespace {
+
+TEST(ManagerTest, TestsWork) { EXPECT_FLOAT_EQ(0.0f, 0.0f); }
+
+}  // namespace
